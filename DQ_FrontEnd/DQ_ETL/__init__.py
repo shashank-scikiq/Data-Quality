e071c418113132b00d5b21d4920e916d3582084d
Shashank Raj <shashank1_theone@yahoo.co.in>
2024-12-17 17:20:21 +0530

    cleaning up frontend code

--- DQ_FrontEnd/DQ_ETL/__init__.py
+++
@@ -1,2 +0,0 @@
-import sys
-sys.path.append("D:/Work/git/DQ_New/FA_Backend/")
--- DQ_FrontEnd/Sql/base_dim_sellers.sql
+++
@@ -1,9 +0,0 @@
-select
-	date(date_parse("O_Created Date & Time", '%Y-%m-%dT%H:%i:%s')) AS order_date,
-	"seller np name" AS seller_np
-from "ATH_DB"."BASE_TABLE"
-where extract(month from date(date_parse("O_Created Date & Time", '%Y-%m-%dT%H:%i:%s'))) = extract(month from date('{date_val}'))
-group by
-	date(date_parse("O_Created Date & Time", '%Y-%m-%dT%H:%i:%s')),
-	"seller np name"
-order by date(date_parse("O_Created Date & Time", '%Y-%m-%dT%H:%i:%s')), "seller np name";
